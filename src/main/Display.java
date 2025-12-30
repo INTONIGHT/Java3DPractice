56216eed8366e68e8a9f010a1db509013345eb90
INTONIGHT <tyler.throop@gmail.com>
2025-12-30 13:57:36 -0700

    starting to create things for the game loop

--- src/main/Display.java
+++ src/main/Display.java
@@ -4,10 +4,43 @@ import java.awt.Canvas;
 
 import javax.swing.JFrame;
 
-public class Display extends Canvas{
+public class Display extends Canvas implements Runnable{
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
 	public static final String TITLE = "Having Fun";
+	
+	private Thread thread;
+	private boolean running = false;
+	
+	public void start() {
+		if(running) {
+			return;
+		}else {
+			running = true;
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	private void stop() {
+		if(!running) {
+			return;
+		}else {
+			running = false;
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+	}
+	
+	public void run() {
+		
+	}
+	
 	//Handles the display and main loop
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
