99c9bb901adb76d78dd719e99134900ccfb2c36e
INTONIGHT <tyler.throop@gmail.com>
2025-12-31 11:34:12 -0700

    starting to improve the logic to the drawing

--- src/main/graphics/Screen.java
+++ src/main/graphics/Screen.java
@@ -18,9 +18,20 @@ public class Screen extends Render{
 	}
 	
 	public void render() {
-		int anim = (int) (Math.sin(System.currentTimeMillis()%2000.0/2000* Math.PI *2) * 200);
-		int anim2 = (int) (Math.cos(System.currentTimeMillis()%2000.0/2000* Math.PI *2) * 200);
-		draw(test, (width - 256)/2 + anim, (height - 256)/2 + anim2);
+		//removes the animation instead of persisting the animation
+		for(int i =0; i<width*height;i++) {
+			pixels[i] = 0;
+		}
+		//this loop lets you draw it more times and gives it a nice flow
+		//if you increase this loop it will start to fry your machine
+		for(int i =0; i<100;i++) {
+			//if you adjust the part after the % you can speed it up or slow it down bigger numbers will slow it down
+			
+			int anim = (int) (Math.sin((System.currentTimeMillis() + i) % 2000.0/2000* Math.PI *2) * 200);
+			int anim2 = (int) (Math.cos((System.currentTimeMillis() + i) % 2000.0/2000* Math.PI *2) * 200);
+			draw(test, (width - 256)/2 + anim, (height - 256)/2 + anim2);
+		}
+		
 	} 
 
 }
