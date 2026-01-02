df17d784bddfbaee944624e0dc82ce5495390a09
INTONIGHT <tyler.throop@gmail.com>
2026-01-02 10:46:24 -0700

    fps counter

--- src/main/graphics/Render.java
+++ src/main/graphics/Render.java
@@ -6,7 +6,7 @@ public class Render {
 	public final int width;
 	public final int height;
 	public final int[] pixels;
-	private static Display display;
+	
 	
 	public Render(int width,int height) {
 		this.width = width;
@@ -17,12 +17,12 @@ public class Render {
 	public void draw(Render render, int xOffset, int yOffset) {
 		for(int y =0; y < render.height;y++) {
 			int yPix = y + yOffset;
-			if(yPix < 0 || yPix >= display.HEIGHT) {
+			if(yPix < 0 || yPix >= Display.HEIGHT) {
 				continue;
 			}
 			for(int x=0; x < render.width;x++) {
 				int xPix = x + xOffset;
-				if(xPix < 0 || xPix >= display.WIDTH) {
+				if(xPix < 0 || xPix >= Display.WIDTH) {
 					continue;
 				}
 				
