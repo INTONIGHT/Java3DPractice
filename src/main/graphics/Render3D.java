f76b979f1bbe7a189467862524d6561678f663dc
INTONIGHT <tyler.throop@gmail.com>
2026-01-09 14:09:51 -0700

    more dcode cleanup

--- src/main/graphics/Render3D.java
+++ src/main/graphics/Render3D.java
@@ -7,19 +7,29 @@ public class Render3D extends Render{
 		// TODO Auto-generated constructor stub
 	}
 	
+	double time = 0;
+	
 	public void floor() {
 		for(int y = 0; y<height;y++) {
 			double ceiling = (y - height/2.0) / height;
+			if(ceiling < 0) {
+				ceiling = -ceiling;
+			}
 			double z = 8 /ceiling;
+			//this lets you have a change to whats being rendered but it will look wonky if
+			//you increment it a lot this is machine dependent so a powerful computer will need smaller increments potentially
+			time += 0.00005;
+			
 			
 			for(int x =0; x<width ;x++) {
 				double xDepth = (x - width / 2.0) / height;
 				xDepth *= z;
 				//using a bitwise operator
 				//can also use << or >> for some interesting effects
-				int intValXDepth = (int) xDepth & 15;
-				int intValZ = (int) z & 15;
-				pixels[x + y * width] = (intValXDepth * 16) | (intValZ * 16) << 8;
+				//subtracting time can get moroe diagonal movemnt
+				int intValXDepth = (int) (xDepth);
+				int intValZ = (int) (z + time);
+				pixels[x + y * width] = ((intValXDepth & 15)* 16 ) | ((intValZ & 15)* 16) << 8;
 			}
 		}
 	}
